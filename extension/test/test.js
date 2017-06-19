44883fdb088c21f06c39a8023e9754211b549b8c
Jeff Larson <thejefflarson@gmail.com>
2017-06-19 12:23:56 -0700

    all tests

--- extension/test/test.js
+++ extension/test/test.js
@@ -9,3 +9,4 @@ let posts = Array.from(dom.window.document.querySelectorAll(".fbUserContent"));
 let ads = posts.map((i) => parser(i, "Sponsored")).filter((i) => i);
 assert.equal(ads.length, 2, "Found two ads");
 assert.equal(ads[0].id, "hyperfeed_story_id_5944756a18bdf2794599305", "Got an ad");
+assert.equal(ads[1].id, "hyperfeed_story_id_594474fe329fb7047789758", "Found the other.");
