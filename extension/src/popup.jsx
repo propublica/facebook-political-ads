9c8b646eb3cdb720cec5c975fa990eafa379cf14
Jeff Larson <thejefflarson@gmail.com>
2017-06-29 11:25:41 -0700

    jsx

---
+++ extension/src/popup.jsx
@@ -0,0 +1,22 @@
+import { h, render } from 'preact';
+import { PropTypes } from 'proptypes';
+
+const Toggler = ({}) => {
+
+};
+
+const Ads = ({}) => {
+
+};
+
+const Rater = ({}) => {
+
+};
+
+render(
+  <div id="popup">
+    <Toggler />
+    <Ads />
+    <Rater />
+  </div>,
+document.body);
