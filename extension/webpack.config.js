c9821aa73515f1bc699f9fb616a329fb9bde1639
Jeff Larson <thejefflarson@gmail.com>
2017-07-05 15:17:40 -0700

    build directory

--- extension/webpack.config.js
+++ extension/webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const webpack = require('webpack');
 const outdir = path.resolve(__dirname, 'dist');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const ENV = process.env.NODE_ENV || 'development';
 
 module.exports = [{
@@ -41,9 +43,17 @@ module.exports = [{
   },
   plugins: [
     new webpack.NoEmitOnErrorsPlugin(),
+    new HTMLWebpackPlugin({
+      title: "__MSG_extension_name__"
+    }),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(ENV)
-    })
+    }),
+    new CopyWebpackPlugin([{
+      from: '../manifest.json', to: outdir
+    },{
+      from: '../_locales', to: outdir + "/_locales"
+    }]),
   ],
   devtool: 'source-map'
 }];
