dd943ea9a5e170292bbbc7e47ff46472ad5227cd
Jeff Larson <thejefflarson@gmail.com>
2017-07-05 17:44:05 -0700

    Grabbing sidebar ads now too

--- extension/src/content.js
+++ extension/src/content.js
@@ -2,13 +2,10 @@ import parser from "parser";
 
 let sendAds = function(){
   let posts = Array.from(document.querySelectorAll(".fbUserContent"));
-
   let sponsor = chrome.i18n.getMessage("sponsor_text");
-
   let ads = posts.map((i) =>  parser(i, sponsor)).filter((i) => i);
-
   chrome.runtime.sendMessage(ads);
 };
 
 let observer = new MutationObserver(sendAds);
-observer.observe(document.querySelector("body"));
+observer.observe(document.querySelector("body"), {childList: true});
