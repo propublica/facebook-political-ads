08e8c92644d9323836da8019828d51b840c3df8d
Jeff Larson <thejefflarson@gmail.com>
2017-07-10 11:39:09 -0700

    ego ads

--- extension/test/test.js
+++ extension/test/test.js
@@ -13,7 +13,7 @@ const parser = require('../src/parser.js');
 let posts = Array.from(dom.window.document.querySelectorAll(".fbUserContent"))
   .concat(Array.from(dom.window.document.querySelectorAll(".ego_unit")));
 let ads = posts.map((i) => parser(i, "Sponsored")).filter((i) => i);
-console.log(ads.length);
-assert.equal(ads.length, 6, "Found six ads");
+
+assert.equal(ads.length, 7, "Found seven ads");
 assert.equal(ads[0].id, "hyperfeed_story_id_5944756a18bdf2794599305", "Got an ad");
 assert.equal(ads[1].id, "hyperfeed_story_id_594474fe329fb7047789758", "Found the other.");
