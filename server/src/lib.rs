246c3c223446dde95c2e8232514df05c734f2773
Jeff Larson <thejefflarson@gmail.com>
2017-07-12 08:57:08 -0700

    server works

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -7,8 +7,6 @@ extern crate hyper;
 extern crate futures;
 extern crate futures_cpupool;
 #[macro_use]
-extern crate lazy_static;
-#[macro_use]
 extern crate log;
 extern crate pretty_env_logger;
 extern crate r2d2;
@@ -28,19 +26,6 @@ use r2d2_diesel::ConnectionManager;
 use r2d2::{Pool, Config};
 use diesel::pg::PgConnection;
 use futures_cpupool::CpuPool;
-
-lazy_static! {
-    pub static ref DB_POOL: Pool<ConnectionManager<PgConnection>> = {
-        dotenv().ok();
-        let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-        let config = Config::default();
-        let manager = ConnectionManager::<PgConnection>::new(database_url);
-        Pool::new(config, manager).expect("Failed to create pool.")
-    };
-    pub static ref THREAD_POOL: CpuPool = {
-        CpuPool::new_num_cpus()  
-    };
-}
 
 #[cfg(test)]
 mod tests {
