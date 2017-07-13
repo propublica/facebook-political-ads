aa1c390ad2b226ecd8dfbc5c0b3841d819c8ee5d
Jeff Larson <thejefflarson@gmail.com>
2017-07-12 19:04:49 -0700

    wrk script

---
+++ server/tools/bench.lua
@@ -0,0 +1,10 @@
+counter = 1
+
+request = function()
+  counter = counter + 1
+  wrk.method = "POST"
+  path = "/ads"
+  wrk.headers["Content-Type"] = "application/x-www-form-urlencoded"
+  wrk.body = '{"id":"test' .. counter .. '", "html":"<div></div>", "political":true}'
+  return wrk.format(nil, path)
+end
