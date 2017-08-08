3d914092cbb7349dfef5cd932cdc96baa6945630
Jeff Larson <thejefflarson@gmail.com>
2017-08-08 11:47:26 -0700

    facebook changes

--- extension/src/content.js
+++ extension/src/content.js
@@ -2,7 +2,7 @@ import parser from 'parser';
 import throttle from 'lodash/throttle';
 
 const sendAds = function(){
-  let posts = Array.from(document.querySelectorAll(".fbUserContent"))
+  let posts = Array.from(document.querySelectorAll(".fbUserContent, .fbUserPost, ._5pcr"))
     .concat(Array.from(document.querySelectorAll(".ego_unit")));
   let ads = posts.map((i) => parser(i)).filter((i) => i);
   chrome.runtime.sendMessage(ads);
--- extension/test/test.js
+++ extension/test/test.js
@@ -10,10 +10,10 @@ global.document = dom.window.document;
 
 const parser = require('../src/parser.js');
 
-let posts = Array.from(dom.window.document.querySelectorAll(".fbUserContent"))
+let posts = Array.from(dom.window.document.querySelectorAll(".fbUserContent, .fbUserPost, ._5pcr"))
   .concat(Array.from(dom.window.document.querySelectorAll(".ego_unit")));
 let ads = posts.map((i) => parser(i)).filter((i) => i);
-
-assert.equal(ads.length, 7, "Found seven ads");
+console.log(ads)
+assert.equal(ads.length, 8, "Found eight ads");
 assert.equal(ads[0].id, "hyperfeed_story_id_5944756a18bdf2794599305", "Got an ad");
 assert.equal(ads[1].id, "hyperfeed_story_id_594474fe329fb7047789758", "Found the other.");
