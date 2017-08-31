902699e9e760a367faee120a00cf565640bfdd31
Jeff Larson <thejefflarson@gmail.com>
2017-08-30 20:48:56 -0700

    cleanup

--- backend/tools/ruby/dumper.rb
+++
@@ -1,8 +0,0 @@
-require 'bundler'
-Bundler.require
-
-conn = PG.connect(:dbname => 'facebook_ads_germany')
-
-res = conn.exec("select html from ads")
-
-puts res.map { |r| Oga.parse_html(r["html"]).children.text }.to_json
