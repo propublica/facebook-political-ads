afeb28195cc09eec1c05f8a73c0413ff5a4b2a4e
Jeff Larson <thejefflarson@gmail.com>
2017-09-01 12:06:56 -0700

    migrations

---
+++ backend/server/migrations/2017-09-01-021623_add_indexes_on_fields/down.sql
@@ -0,0 +1,2 @@
+drop index index_ads_on_lang;
+drop index index_ads_on_political_probability;
---
+++ backend/server/migrations/2017-09-01-021623_add_indexes_on_fields/up.sql
@@ -0,0 +1,2 @@
+create index index_ads_on_lang on ads(lang);
+create index index_ads_on_political_probability on ads(political_probability);
