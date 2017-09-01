f2ecb767dadad2311d1793f9a9610508088fe873
Jeff Larson <thejefflarson@gmail.com>
2017-08-31 18:57:38 -0700

    we can probably serve ads now

--- backend/classifier/src/lib.rs
+++ backend/classifier/src/lib.rs
@@ -3,7 +3,6 @@ extern crate error_chain;
 extern crate murmurhash3;
 extern crate regex;
 extern crate rusty_machine;
-extern crate serde;
 extern crate serde_json;
 #[macro_use]
 extern crate serde_derive;
