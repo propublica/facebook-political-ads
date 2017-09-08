a98788d36c3809e10d83a00de4a47d9bcde85d73
Jeff Larson <thejefflarson@gmail.com>
2017-09-07 17:21:45 -0700

    ok more dashboard

--- backend/server/public/src/admin.js
+++ backend/server/public/src/admin.js
@@ -5,6 +5,7 @@ import { createLogger } from 'redux-logger';
 
 const NEW_ADS = "new_ads";
 const HIDE_AD = "hide_ad";
+const LOGIN = "login";
 
 const hideAd = (ad) => ({
   type: HIDE_AD,
@@ -15,6 +16,33 @@ const newAds = (ads) => ({
   type: NEW_ADS,
   value: ads
 });
+
+const login = (credentials) => ({
+  type: LOGIN,
+  value: credentials
+});
+
+const credentials = (state = {}, action) => {
+  switch(action.type) {
+  case LOGIN:
+    return action.value;
+  default:
+    return state;
+  }
+};
+
+const login = (username, password) => {
+  // create jwt
+  return (dispatch) => {
+    const token = { username, password };
+    return fetch("/facebook-ads/login", {
+      method: "POST",
+      headers: new Headers({
+        "Authorization": "Bearer " + token
+      }).then(() => dispatch(login(token)))
+    });
+  };
+};
 
 const ads = (state = [], action) => {
   switch(action.type) {
@@ -33,15 +61,19 @@ const ads = (state = [], action) => {
 };
 
 const suppressAd = (ad) => {
-  return (dispatch) => {
+  return (dispatch, getState) => {
     let body = {
       ...ad,
       suppressed: true
     };
     dispatch(hideAd(ad));
+
     return fetch("/facebook-ads/suppress", {
       method: "POST",
-      body: JSON.stringify(body)
+      body: JSON.stringify(body),
+      headers: new Headers({
+        "Authorization": "Bearer " + getState().credentials
+      })
     }).then((resp) => resp.json())
       .then((ads) => dispatch(newAds(ads)));
   };
