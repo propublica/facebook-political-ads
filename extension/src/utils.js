675ee81bc606f7abcb121bbc14e5dd4687ad2a2f
Jeff Larson <thejefflarson@gmail.com>
2017-09-22 16:47:30 -0700

    allow the user to double check their language and country settings.

--- extension/src/utils.js
+++ extension/src/utils.js
@@ -7,13 +7,22 @@ const adForRequest = (ad) => ({
 const endpoint = process.env.NODE_ENV === 'production' ?
   "https://projects.propublica.org/facebook-ads/ads" :
   "http://0.0.0.0:8080/facebook-ads/ads";
-const sendAds = (body) => fetch(endpoint, {
+
+const headers = (language) => new Headers({
+  "Accept-Language": language.language + "-" + language.country
+});
+
+const sendAds = (body, language) => fetch(endpoint, {
   method: "POST",
   mode: 'no-cors',
+  headers: headers(language),
   body: JSON.stringify(body)
 });
 
-const getAds = (cb) => fetch(endpoint).then((res) => res.json()).then(cb);
+const getAds = (language, cb) => fetch(endpoint, {
+  method: "GET",
+  headers: headers(language),
+}).then((res) => res.json()).then(cb);
 
 const mergeAds = (ads, newAds) => {
   let ids = new Map(ads.map(ad => [ad.id, ad]));
