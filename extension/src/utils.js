4160d2014cdcaa09b94fb9b3e2ff98a52136d54e
Jeff Larson <thejefflarson@gmail.com>
2017-10-19 16:32:29 -0700

    fix sorting bug

--- extension/src/utils.js
+++ extension/src/utils.js
@@ -29,7 +29,6 @@ const getAds = (language, cb) => fetch(endpoint, {
 const mergeAds = (ads, newAds) => {
   let ids = new Map(ads.map(ad => [ad.id, ad]));
   newAds.forEach(ad => {
-    ad = Object.assign({}, {created_at: (new Date()).valueOf()}, ad);
     if(ids.has(ad.id)) {
       let old = ids.get(ad.id);
       let newAd = Object.assign({}, old, ad);
