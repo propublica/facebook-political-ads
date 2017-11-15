6c2bd9404a6cd8fca1d3cf38209f249d3089bb13
Jeff Larson <thejefflarson@gmail.com>
2017-11-14 17:53:06 -0800

    can parse most us ads, now onto the germans

--- backend/server/src/targeting_parser.rs
+++ backend/server/src/targeting_parser.rs
@@ -1,5 +1,5 @@
 use errors::*;
-use nom::{IResult, rest_s};
+use nom::IResult;
 
 #[derive(Clone, Debug, PartialEq)]
 pub enum Targeting<'a> {
@@ -10,29 +10,72 @@ pub enum Targeting<'a> {
     Age(&'a str),
     Interest(&'a str),
     Segment(&'a str),
+    Advertiser(&'a str),
+    Retargeting(&'a str),
+    Agency(&'a str),
+    Website(&'a str),
 }
 
-named!(interest(&str) -> Vec<Targeting>,
+named!(until_b(&str) -> &str, take_until!("</b>"));
+
+named!(provider(&str) -> Targeting,
     do_parse!(
-        interest: rest_s >>
-        (vec![Targeting::Interest(interest)])
+        take_until_and_consume!("wants to reach people who are part of an audience") >>
+        take_until_and_consume!("<b>") >>
+        agency: until_b >> (Targeting::Agency(agency))
     )
 );
 
-named!(segment(&str) -> Vec<Targeting>,
+named!(advertiser(&str) -> Targeting,
+    do_parse!(
+        take_until_and_consume!("<b id=\"ad_prefs_advertiser\">") >>
+        advertiser: until_b >>
+        (Targeting::Advertiser(advertiser))
+    )
+);
+
+named!(interest(&str) -> Targeting,
+   do_parse!(
+        take_until_and_consume!("<b id=\"ad_prefs_interest\">") >>
+        interest: until_b >>
+        (Targeting::Interest(interest))
+    )
+);
+
+named!(segment(&str) -> Targeting,
     do_parse!(
         tag!("\"") >>
         segment: take_until!("\"") >>
         tag!("\"") >>
-        (vec![Targeting::Segment(segment)])
+        (Targeting::Segment(segment))
     )
 );
 
 named!(gender(&str) -> Option<Targeting>,
-    alt!(
-        tag!("men") => {|_| Some(Targeting::Gender("Men")) } |
-        tag!("women") => {|_| Some(Targeting::Gender("Women")) } |
-        tag!("people") => {|_| None }
+    do_parse!(
+        take_until_and_consume!("<b>") >>
+        result: alt!(
+            tag!("men") => {|_| Some(Targeting::Gender("Men")) } |
+            tag!("women") => {|_| Some(Targeting::Gender("Women")) } |
+            tag!("people") => {|_| None }
+        ) >>
+        (result)
+    )
+);
+
+named!(website(&str) -> Targeting,
+   do_parse!(
+       take_until_and_consume!("wants to reach ") >>
+       res: tag!("people who have visited their website or used one of their apps") >>
+       (Targeting::Website(res))
+   )
+);
+
+named!(retargeting(&str) -> Targeting,
+    do_parse!(
+        take_until_and_consume!("<b>") >>
+        res: tag!("people who may be similar to their customers") >>
+        (Targeting::Retargeting(res))
     )
 );
 
@@ -40,35 +83,42 @@ named!(age(&str) -> Option<Targeting>,
     do_parse!(
         ws!(tag!("age")) >>
         ws!(opt!(tag!("s"))) >>
+        ws!(opt!(tag!("d"))) >>
         complete: ws!(take_until_and_consume!(" who")) >>
         (Some(Targeting::Age(complete)))
     )
 );
 
 named!(country(&str) -> Vec<Option<Targeting>>,
-    do_parse!(country: rest_s >> (vec![Some(Targeting::Region(country))]))
+    do_parse!(country: until_b >> (vec![Some(Targeting::Region(country))]))
 );
 
 named!(city_state(&str) -> Vec<Option<Targeting>>,
     do_parse!(
         ws!(alt!(take_until_and_consume!("near") | take_until_and_consume!("in"))) >>
         city: ws!(take_until_and_consume!(",")) >>
-        state: rest_s >>
+        state: until_b >>
         (vec![Some(Targeting::City(city)), Some(Targeting::State(state))])
     )
 );
 
-named!(gender_age_and_location(&str) -> Vec<Targeting>,
+named!(age_and_location(&str) -> Vec<Option<Targeting>>,
     do_parse!(
-        g: ws!(gender) >>
         a: ws!(age) >>
         l: ws!(alt!(city_state | country)) >>
-        ([&vec![g, a][..], &l[..]].concat().into_iter().filter_map(|x| x).collect())
+        (vec![&vec![a][..], &l[..]].concat())
     )
 );
 
 named!(targeting(&str) -> Vec<Targeting>,
-    alt!(gender_age_and_location | segment | interest)
+    do_parse!(
+        advertiser: dbg!(advertiser) >>
+        sources_and_interests: dbg!(alt!(interest | retargeting | website | provider)) >>
+        gender: dbg!(gender) >>
+        location: dbg!(age_and_location) >>
+        (vec![&vec![Some(advertiser), Some(sources_and_interests), gender][..], &location[..]]
+              .concat().iter().filter_map(|x| x.clone()).collect())
+    )
 );
 
 fn parse_targeting(thing: &str) -> Result<Vec<Targeting>> {
@@ -87,32 +137,39 @@ mod tests {
     fn test_targeting() {
         assert_eq!(
             segment("\"Generation X\""),
-
-            IResult::Done("", vec![Targeting::Segment("Generation X")])
+            IResult::Done("", Targeting::Segment("Generation X"))
+        );
+        let g = "Google";
+        assert_eq!(
+            advertiser(&format!("<b id=\"ad_prefs_advertiser\">{}</b>", g)),
+            IResult::Done("</b>", Targeting::Advertiser(g))
         );
         let i = "American Federation of State, County and Municipal Employees";
-        assert_eq!(interest(i), IResult::Done("", vec![Targeting::Interest(i)]));
         assert_eq!(
-            gender("men"),
+            interest(&format!("<b id=\"ad_prefs_interest\">{}</b>", i)),
+            IResult::Done("</b>", Targeting::Interest(i))
+        );
+        assert_eq!(
+            gender("<b>men"),
             IResult::Done("", Some(Targeting::Gender("Men")))
         );
         assert_eq!(
-            gender("women"),
+            gender("<b>women"),
             IResult::Done("", Some(Targeting::Gender("Women")))
         );
-        assert_eq!(gender("people"), IResult::Done("", None));
+        assert_eq!(gender("<b>people"), IResult::Done("", None));
         assert_eq!(
             age(" ages 26 to 62 who "),
             IResult::Done("", Some(Targeting::Age("26 to 62")))
         );
         assert_eq!(
-            country("United States"),
-            IResult::Done("", vec![Some(Targeting::Region("United States"))])
+            country("United States</b>"),
+            IResult::Done("</b>", vec![Some(Targeting::Region("United States"))])
         );
         assert_eq!(
-            city_state("near Burlington, North Carolina"),
+            city_state("near Burlington, North Carolina</b>"),
             IResult::Done(
-                "",
+                "</b>",
                 vec![
                     Some(Targeting::City("Burlington")),
                     Some(Targeting::State("North Carolina")),
@@ -120,20 +177,45 @@ mod tests {
             )
         );
         assert_eq!(
-            targeting(
-                "people ages 18 and older who live or were recently near San Francisco, California",
+            age_and_location(
+                "ages 18 and older who live or were recently near San Francisco, California</b>",
             ),
             IResult::Done(
-                "",
+                "</b>",
                 vec![
-                    Targeting::Age("18 and older"),
-                    Targeting::City("San Francisco"),
-                    Targeting::State("California"),
+                    Some(Targeting::Age("18 and older")),
+                    Some(Targeting::City("San Francisco")),
+                    Some(Targeting::State("California")),
                 ],
             )
         );
     }
 
     #[test]
-    fn test_all() {}
+    fn test_all() {
+        use dotenv::dotenv;
+        use diesel::prelude::*;
+        use diesel::pg::PgConnection;
+        use models::Ad;
+        use schema::ads::dsl::*;
+        use std::env;
+        dotenv().ok();
+        let database_url = env::var("DATABASE_URL").unwrap();
+        let connection = PgConnection::establish(&database_url).unwrap();
+        let adverts = ads.filter(targeting.is_not_null())
+            .limit(10)
+            .load::<Ad>(&connection)
+            .unwrap();
+
+        for ad in adverts {
+            let t = ad.clone().targeting.unwrap();
+            let targets = parse_targeting(&t);
+
+            if targets.is_err() {
+                println!("{:?}", targets);
+                println!("{:?}", ad.clone().targeting);
+                assert!(false)
+            }
+        }
+    }
 }
