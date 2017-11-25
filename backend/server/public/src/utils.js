7034670e3950fd24267fdabc7b349a13aa285c75
Surya Mattu <surya@suryamattu.com>
2017-11-25 18:18:32 -0500

    pagination wokring; added last page check;

--- backend/server/public/src/utils.js
+++ backend/server/public/src/utils.js
@@ -22,32 +22,55 @@ const newAds = (ads) => ({
 
 const PAGE_NEXT = 'PAGE_NEXT';
 const PAGE_PREV = 'PAGE_PREV';
+// SM: Guessing we need this for clearing pages when you search. Haven't implemneted yet.
+const PAGE_CLEAR = 'PAGE_CLEAR';
 
 const pageCount = {
   pageNext() {
     return { type: PAGE_NEXT };
   },
-
   pagePrev() {
     return { type: PAGE_PREV };
+  },
+  pageClear() {
+    return { type: PAGE_CLEAR}
   }
 }
+
+const IS_LAST_PAGE = 'IS_LAST_PAGE';
+const NOT_LAST_PAGE = 'NOT_LAST_PAGE';
+
+const isLastPage = () => ({
+  type: IS_LAST_PAGE
+});
+
+const notLastPage = () => ({
+  type: NOT_LAST_PAGE
+})
 
 const refresh = (store, query, page) => {
   let url = "/facebook-ads/ads";
   if(query) {
     url = url + `?search=${query}`;
   }
-  if(page) {
-    url = url + `?page=${page}`;
+  if (store.getState().pageIndex) {
+    url = url + `?page=${store.getState().pageIndex}`;
   }
+
   return fetch(url, {
     method: "GET",
     headers: headers(store.getState().credentials)
   }).then((res) => res.json())
-    .then((ads) => { store.dispatch(newAds(ads.ads)); });
+    .then((ads) => {
+      if (store.getState().pageIndex > 4) {
+        store.dispatch(isLastPage());
+      } else {
+        store.dispatch(notLastPage());
+      }
+      store.dispatch(newAds(ads.ads));
+    });
 };
 
 const search = (store, query) => () => refresh(store, query, 0);
 
-export { headers, newAds, NEW_ADS, pageCount, PAGE_NEXT, PAGE_PREV, refresh, search };
+export { headers, newAds, NEW_ADS, pageCount, PAGE_NEXT, PAGE_PREV, PAGE_CLEAR, IS_LAST_PAGE, NOT_LAST_PAGE, refresh, search };
