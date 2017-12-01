48c33efccfac6e5dd8905b215386fe1bb8345bb9
Jeff Larson <thejefflarson@gmail.com>
2017-11-30 18:07:54 -0800

    targeting!

--- backend/server/src/schema.rs
+++ backend/server/src/schema.rs
@@ -1,16 +1,1 @@
 infer_schema!("dotenv:DATABASE_URL");
-
-// Dummy tables for derived queries
-table! {
-    use diesel::types::{BigInt, Text};
-    aggregate_targetings (count) {
-        count -> BigInt,
-        targeting_type -> Text,
-    }
-}
-
-#[derive(QueryableByName, Debug, Clone)]
-pub struct AggregateTargeting {
-    pub count: i64,
-    pub targeting_type: String,
-}
