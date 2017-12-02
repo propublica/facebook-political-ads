de8af5cc8cdd690107c3a07003b5fe387500246b
Jeff Larson <thejefflarson@gmail.com>
2017-12-01 22:55:02 -0800

    mostly there

--- backend/server/public/src/pagination.js
+++ backend/server/public/src/pagination.js
@@ -1,6 +1,6 @@
-const PAGE_NEXT = 'PAGE_NEXT';
-const PAGE_PREV = 'PAGE_PREV';  
-const PAGE_CLEAR = 'PAGE_CLEAR';
+const PAGE_NEXT = 'page_next';
+const PAGE_PREV = 'page_prev';
+const PAGE_CLEAR = 'page_clear';
 
 const pageCount = {
   pageNext() {
@@ -10,11 +10,11 @@ const pageCount = {
     return { type: PAGE_PREV };
   },
   pageClear() {
-    return { type: PAGE_CLEAR}
+    return { type: PAGE_CLEAR };
   }
-}
-const IS_LAST_PAGE = 'IS_LAST_PAGE';
-const NOT_LAST_PAGE = 'NOT_LAST_PAGE';
+};
+const IS_LAST_PAGE = 'is_last_page';
+const NOT_LAST_PAGE = 'not_last_page';
 
 const isLastPage = () => ({
   type: IS_LAST_PAGE
@@ -22,32 +22,34 @@ const isLastPage = () => ({
 
 const notLastPage = () => ({
   type: NOT_LAST_PAGE
-})
+});
 
 const lastPage = (state = false, action) => {
   switch(action.type) {
-    case IS_LAST_PAGE:
-      return true;
-    case NOT_LAST_PAGE:
-      return false;
-    default:
-      return state;
+  case IS_LAST_PAGE:
+    return true;
+  case NOT_LAST_PAGE:
+    return false;
+  default:
+    return state;
   }
-}
+};
+
 const pageIndex = (state = 0, action) => {
   switch(action.type) {
-    case PAGE_NEXT:
-      return state + 1;
-
-    case PAGE_PREV:
+  case PAGE_NEXT:
+    return state + 1;
+  case PAGE_PREV:
+    if ((state - 1) > 0) {
       return state - 1;
-
-    case PAGE_CLEAR:
+    } else {
       return 0;
-
-    default:
-      return state;
+    }
+  case PAGE_CLEAR:
+    return 0;
+  default:
+    return state;
   }
-}
+};
 
 export { pageCount, lastPage, pageIndex, isLastPage, notLastPage} ;
