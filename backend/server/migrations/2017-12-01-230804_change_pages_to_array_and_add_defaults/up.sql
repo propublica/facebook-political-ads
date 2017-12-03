fffcaa04afd78c7bc81ff7498f6546e16000d7ba
Jeff Larson <thejefflarson@gmail.com>
2017-12-02 20:47:38 -0800

    fix migration

--- backend/server/migrations/2017-12-01-230804_change_pages_to_array_and_add_defaults/up.sql
+++ backend/server/migrations/2017-12-01-230804_change_pages_to_array_and_add_defaults/up.sql
@@ -1,7 +1,7 @@
 alter table ads alter column entities set default '[]'::jsonb;
 alter table ads alter column targets set default '[]'::jsonb;
 alter table ads alter column pages drop default;
-alter table ads alter column pages type text array using [];
-alter table ads alter column pages set default text[];
+alter table ads alter column pages type text array using array[pages];
+alter table ads alter column pages set default '{}';
 update ads set entities = '[]'::jsonb;
 update ads set targets = '[]'::jsonb;
