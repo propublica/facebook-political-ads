0ad9653219880199b90819e23bf3a9288372adf4
Jeff Larson <thejefflarson@gmail.com>
2017-12-03 18:26:49 -0800

    translations and more filters

--- backend/server/migrations/2017-12-01-230804_change_pages_to_array_and_add_defaults/up.sql
+++ backend/server/migrations/2017-12-01-230804_change_pages_to_array_and_add_defaults/up.sql
@@ -3,6 +3,5 @@ alter table ads alter column targets set default '[]'::jsonb;
 alter table ads alter column pages drop default;
 alter table ads alter column pages type text array using array[]::text[];
 alter table ads alter column pages set default '{}';
-update ads set pages = array[]::text[];
 update ads set entities = '[]'::jsonb;
 update ads set targets = '[]'::jsonb;
