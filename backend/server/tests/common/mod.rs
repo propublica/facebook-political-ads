1fd9ac35d598ff16f56a71184b18eeffc93233ea
Ben Smithgall <ben.smithgall@gmail.com>
2017-12-28 14:15:19 -0500

    create integration tests for database interaction

---
+++ backend/server/tests/common/mod.rs
@@ -0,0 +1,81 @@
+extern crate server;
+
+use dotenv::dotenv;
+use std::env;
+use std::str::FromStr;
+use diesel::prelude::*;
+use diesel;
+use diesel::pg::PgConnection;
+
+use chrono::offset::Utc;
+
+use rand::{thread_rng, Rng};
+
+use r2d2_diesel::ConnectionManager;
+use r2d2::Pool;
+
+use server::models::*;
+use server::schema::ads;
+use server::schema::ads::dsl::*;
+use server::server::AdPost;
+
+pub fn connect() -> PgConnection {
+    dotenv().ok();
+    let database_url = env::var("TEST_DATABASE_URL").unwrap();
+    PgConnection::establish(&database_url).unwrap()
+}
+
+pub fn connect_pool() -> Pool<ConnectionManager<PgConnection>> {
+    dotenv().ok();
+    let database_url = env::var("TEST_DATABASE_URL").unwrap();
+    let manager = ConnectionManager::<PgConnection>::new(database_url.clone());
+    Pool::builder().build(manager).expect("Failed to create pool.")
+}
+
+pub fn seed(connection: &PgConnection) {
+    unseed(connection);
+
+    let mock_html = include_str!("html-test.txt");
+    let en = String::from("en-US");
+
+    let post: AdPost = AdPost {
+        id: "1".to_string(),
+        html: mock_html.to_string(),
+        political: None,
+        targeting: Some(make_targeting())
+    };
+
+    let new_ad: NewAd = NewAd::new(&post, &en).unwrap();
+
+    let _ = diesel::insert_into(ads::table)
+        .values(&new_ad)
+        .on_conflict(id)
+        .do_update()
+        .set((updated_at.eq(Utc::now()),))
+        .execute(connection)
+        .unwrap();
+}
+
+pub fn seed_political(connection: &PgConnection) {
+    seed(connection);
+
+    let _ = diesel::update(ads.find("1".to_string()))
+        .set((political_probability.eq(1.0)))
+        .execute(connection)
+        .unwrap();
+}
+
+pub fn unseed(connection: &PgConnection) {
+    diesel::delete(ads).execute(&*connection).expect("Error unseeding");
+}
+
+fn make_targeting() -> String {
+    let mut rng = thread_rng();
+
+    let targetings = include_str!("targetings.txt")
+        .lines()
+        .map(|s| String::from_str(s).expect("Error reading string"))
+        .collect::<Vec<_>>();
+
+    return rng.choose(&targetings).unwrap().clone();
+}
