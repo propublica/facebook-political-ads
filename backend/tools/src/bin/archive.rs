1fd4b101b9b3ddbbad66b11126c079499f2cdf6f
Jeff Larson <thejefflarson@gmail.com>
2018-01-16 17:30:53 -0800

    archive.rs

---
+++ backend/tools/src/bin/archive.rs
@@ -0,0 +1,29 @@
+// extern crate csv;
+// extern crate diesel;
+// #[macro_use]
+// extern crate serde_derive;
+
+// #[derive(Debug, Serialize)]
+// struct Record {
+//     pub id: String,
+//     pub html: String,
+//     pub political: i32,
+//     pub not_political: i32,
+//     pub title: String,
+//     pub message: String,
+//     pub thumbnail: String,
+//     pub created_at: DateTime<Utc>,
+//     pub updated_at: DateTime<Utc>,
+//     pub lang: String,
+//     pub images: Vec<String>,
+//     pub impressions: i32,
+//     pub political_probability: f64,
+//     pub targeting: Option<String>,
+//     #[serde(skip_serializing)]
+//     pub suppressed: bool,
+//     pub targets: Option<String>,
+//     pub advertiser: Option<String>,
+//     pub entities: Option<String>,
+//     pub page: Option<String>,
+// }
+fn main() {}
