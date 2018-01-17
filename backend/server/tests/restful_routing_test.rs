a7d5f845d5a90f3b93eccae8af268485bbb63d5f
Jeremy B. Merrill <jeremy.merrill@propublica.org>
2018-01-17 17:41:57 -0500

    fixes tests

--- backend/server/tests/restful_routing_test.rs
+++ backend/server/tests/restful_routing_test.rs
@@ -8,16 +8,23 @@ extern crate server;
 extern crate tokio_core;
 
 use futures::{Future, Stream};
-use hyper::Client;
+use hyper::{Client, Method, Request};
 use tokio_core::reactor::Core;
 use serde_json::Value;
+use hyper::header::{qitem, AcceptLanguage, LanguageTag};
 
 #[test]
 fn test_index_url() {
     let mut core = Core::new().unwrap();
     let client = Client::new(&core.handle());
     let uri = "http://localhost:8080/facebook-ads/ads".parse().unwrap();
-    let work = client.get(uri).and_then(|res| {
+    let mut req = Request::new(Method::Get, uri);
+    let mut langtag: LanguageTag = Default::default();
+    langtag.language = Some("en".to_owned());
+    langtag.region = Some("US".to_owned());
+    req.headers_mut().set(AcceptLanguage(vec![qitem(langtag)]));
+
+    let work = client.request(req).and_then(|res| {
         res.body().concat2().and_then(move |body| {
             let v: Value = serde_json::from_slice(&body).unwrap();
             assert!(v["ads"].as_array().unwrap().len() >= 1);
@@ -32,7 +39,13 @@ fn test_index_url_with_slash() {
     let mut core = Core::new().unwrap();
     let client = Client::new(&core.handle());
     let uri = "http://localhost:8080/facebook-ads/ads/".parse().unwrap();
-    let work = client.get(uri).and_then(|res| {
+    let mut req = Request::new(Method::Get, uri);
+    let mut langtag: LanguageTag = Default::default();
+    langtag.language = Some("en".to_owned());
+    langtag.region = Some("US".to_owned());
+    req.headers_mut().set(AcceptLanguage(vec![qitem(langtag)]));
+
+    let work = client.request(req).and_then(|res| {
         res.body().concat2().and_then(move |body| {
             let v: Value = serde_json::from_slice(&body).unwrap();
             assert!(v["ads"].as_array().unwrap().len() >= 1);
@@ -49,7 +62,15 @@ fn test_individual_ad_url() {
     let uri = "http://localhost:8080/facebook-ads/ads/23842729393560096"
         .parse()
         .unwrap();
-    let work = client.get(uri).and_then(|res| {
+
+    let mut req = Request::new(Method::Get, uri);
+    let mut langtag: LanguageTag = Default::default();
+    langtag.language = Some("en".to_owned());
+    langtag.region = Some("US".to_owned());
+    req.headers_mut().set(AcceptLanguage(vec![qitem(langtag)]));
+
+
+    let work = client.request(req).and_then(|res| {
         res.body().concat2().and_then(move |body| {
             let v: Value = serde_json::from_slice(&body).unwrap();
             assert_eq!(v["ads"].as_array().unwrap().len(), 1);
