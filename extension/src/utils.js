6d515f938e5d0d60cc7dd698f20e82993a48837f
Jeff Larson <thejefflarson@gmail.com>
2018-01-24 11:43:51 -0800

    limit stored ads

--- extension/src/utils.js
+++ extension/src/utils.js
@@ -1,35 +1,41 @@
 import { sortBy } from "lodash";
 
-const adForRequest = (ad) => ({
+const adForRequest = ad => ({
   id: ad.id,
   html: ad.html,
   targeting: ad.targeting
 });
 
-const endpoint = process.env.NODE_ENV === 'production' ?
-  "https://projects.propublica.org/facebook-ads/ads" :
-  "https://projects.propublica.org/facebook-ads/ads";
+const endpoint =
+  process.env.NODE_ENV === "production"
+    ? "https://projects.propublica.org/facebook-ads/ads"
+    : "https://localhost:8080/facebook-ads/ads";
 
-const headers = (language) => new Headers({
-  "Accept-Language": language.language + "-" + language.country + ";q=1.0"
-});
+const headers = language =>
+  new Headers({
+    "Accept-Language": language.language + "-" + language.country + ";q=1.0"
+  });
 
-const sendAds = (body, language) => fetch(endpoint, {
-  method: "POST",
-  mode: 'no-cors',
-  headers: headers(language),
-  body: JSON.stringify(body)
-});
+const sendAds = (body, language) =>
+  fetch(endpoint, {
+    method: "POST",
+    mode: "no-cors",
+    headers: headers(language),
+    body: JSON.stringify(body)
+  });
 
-const getAds = (language, cb) => fetch(endpoint, {
-  method: "GET",
-  headers: headers(language),
-}).then((res) => res.json()).then((ads) => cb(ads.ads || ads));
+const getAds = (language, cb) =>
+  fetch(endpoint, {
+    method: "GET",
+    headers: headers(language)
+  })
+    .then(res => res.json())
+    .then(ads => cb(ads.ads || ads));
 
 const mergeAds = (ads, newAds) => {
   let ids = new Map(ads.map(ad => [ad.id, ad]));
   newAds.forEach(ad => {
-    if(ids.has(ad.id)) {
+    if (ids.has(ad.id)) {
       let old = ids.get(ad.id);
       let newAd = Object.assign({}, old, ad);
       ids.set(newAd.id, newAd);
@@ -37,16 +43,19 @@ const mergeAds = (ads, newAds) => {
       ids.set(ad.id, ad);
     }
   });
-  const idSort = sortBy(Array.from(ids.values()), (a) => -1 * Date.parse(a.created_at));
-  return sortBy(idSort, (a) => a.rating === "political").slice(0, 200);
+  const idSort = sortBy(
+    Array.from(ids.values()),
+    a => -1 * Date.parse(a.created_at)
+  );
+  return sortBy(idSort, a => a.rating === "political").slice(0, 100);
 };
 
-const updateBadge = (ratings) => {
+const updateBadge = ratings => {
   const num = ratings.filter(rating => !("rating" in rating)).length;
-  if(num > 0) {
-    chrome.browserAction.setBadgeText({text: num > 100 ? "100+" : "" + num});
+  if (num > 0) {
+    chrome.browserAction.setBadgeText({ text: num > 100 ? "100+" : "" + num });
   } else {
-    chrome.browserAction.setBadgeText({text: ""});
+    chrome.browserAction.setBadgeText({ text: "" });
   }
 };
 
@@ -55,4 +64,11 @@ const getBrowserLocale = () => ({
   country: navigator.language.split("-")[1]
 });
 
-export { sendAds, getAds, mergeAds, updateBadge, adForRequest, getBrowserLocale };
+export {
+  sendAds,
+  getAds,
+  mergeAds,
+  updateBadge,
+  adForRequest,
+  getBrowserLocale
+};
