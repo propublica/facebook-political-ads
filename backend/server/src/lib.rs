0a418620aa502af74d960617687f70e571c0644e
Jeremy B. Merrill <jeremy.merrill@propublica.org>
2018-01-24 11:26:25 -0500

    adds regex crate; deps automatically alphabetized

--- backend/server/src/lib.rs
+++ backend/server/src/lib.rs
@@ -1,37 +1,39 @@
-#![recursion_limit="256"]
+#![recursion_limit = "256"]
 extern crate chrono;
 #[macro_use]
 extern crate diesel;
+extern crate diesel_full_text_search;
 #[macro_use]
 extern crate diesel_infer_schema;
-extern crate diesel_full_text_search;
 extern crate dotenv;
 #[macro_use]
 extern crate error_chain;
-extern crate hyper;
-extern crate hyper_tls;
 extern crate futures;
 extern crate futures_cpupool;
+extern crate hyper;
+extern crate hyper_tls;
 extern crate jsonwebtoken;
 extern crate kuchiki;
+extern crate log4rs;
 #[macro_use]
 extern crate log;
-extern crate log4rs;
 #[macro_use]
 extern crate nom;
 extern crate r2d2;
 extern crate r2d2_diesel;
+extern crate regex;
 extern crate rusoto_core;
 extern crate rusoto_credential;
 extern crate rusoto_s3;
-extern crate serde_json;
 #[macro_use]
 extern crate serde_derive;
+extern crate serde_json;
 extern crate tokio_core;
 extern crate tokio_postgres;
 extern crate tokio_timer;
 extern crate unicase;
 extern crate url;
+
 
 pub mod errors;
 pub mod models;
@@ -50,9 +52,7 @@ pub fn start_logging() {
         .appender(Appender::builder().build("stdout", Box::new(stdout)))
         .logger(Logger::builder().build("hyper", LevelFilter::Info))
         .logger(Logger::builder().build("server", LevelFilter::Info))
-        .build(Root::builder().appender("stdout").build(
-            LevelFilter::Error,
-        ))
+        .build(Root::builder().appender("stdout").build(LevelFilter::Error))
         .expect("Log config didn't work");
     log4rs::init_config(config).expect("Logging encountered an error.");
 }
