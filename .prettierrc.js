4691105603534296c8e2578f02cf85e4deb7f78e
Jeff Larson <thejefflarson@gmail.com>
2018-01-29 15:23:28 -0800

    We don't have that path.

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,18 +1,7 @@
-const {esNextPaths} = require('./scripts/shared/pathsByLanguageVersion');
-
 module.exports = {
   bracketSpacing: false,
   singleQuote: true,
   jsxBracketSameLine: true,
   trailingComma: 'es5',
   printWidth: 80,
-
-  overrides: [
-    {
-      files: esNextPaths,
-      options: {
-        trailingComma: 'all',
-      },
-    },
-  ],
 };
