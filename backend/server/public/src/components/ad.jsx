4a2578b340765eccb0c4b920d053a33c7fd157aa
Jeremy B. Merrill <jeremy.merrill@propublica.org>
2018-02-07 14:39:34 -0500

    permalinks on the frontend; but there's no link to them yet

--- backend/server/public/src/components/ad.jsx
+++ backend/server/public/src/components/ad.jsx
@@ -1,12 +1,40 @@
 import React from "react";
 import Targeting from "./targeting.jsx";
+import { getOneAd } from "../actions.js";
+import { connect } from "react-redux";
 
-const Ad = ({ ad }) => (
-  <div>
-    <div className="message">
-      <div dangerouslySetInnerHTML={{ __html: ad.html }} />
-    </div>
-    {ad.targeting !== null ? <Targeting targeting={ad.targeting} /> : ""}
-  </div>
-);
+class Ad extends React.Component {
+  componentDidMount() {
+    // if this.props.ad, we're good, we got the ad prop from <Frontend> or it's already been fetched.
+    // if !this.props.ad && !this.props.getOneAd, then something has gone wrong.
+    if (!this.props.ad.id && this.props.getOneAd && this.props.match) {
+      let ad_id = null;
+      // `match` is from React Router -- it's the bit of the URL that matches.
+      ad_id = this.props.match.params.ad_id;
+      console.log("match", ad_id);
+      this.props.getOneAd(ad_id);
+    }
+  }
+
+  render() {
+    return this.props.ad ? (
+      <div>
+        <div className="message">
+          <div dangerouslySetInnerHTML={{ __html: this.props.ad.html }} />
+        </div>
+        {this.props.ad.targeting !== null ? (
+          <Targeting targeting={this.props.ad.targeting} />
+        ) : (
+          ""
+        )}
+      </div>
+    ) : null;
+  }
+}
+
+export const AdRedux = connect(
+  ({ ad }) => ({ ad }),
+  dispatch => ({ getOneAd: ad => dispatch(getOneAd(ad)) })
+)(Ad);
+
 export default Ad;
