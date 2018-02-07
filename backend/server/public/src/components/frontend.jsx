28b389191e08e4bfada4d7dc744d1edd348ead8a
Jeremy B. Merrill <jeremy.merrill@propublica.org>
2018-02-07 14:39:34 -0500

    permalinks on the frontend; but there's no link to them yet

--- backend/server/public/src/components/frontend.jsx
+++ backend/server/public/src/components/frontend.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
-import Ad from "components/ad.jsx";
+import { Route, Switch } from "react-router-dom";
+import Ad, { AdRedux } from "components/ad.jsx";
 import { Filters } from "components/filters.jsx";
 import Pagination from "components/pagination.jsx";
 import Term from "components/term.jsx";
@@ -11,32 +12,45 @@ import { t } from "i18n.js";
 let Frontend = ({ ads, onKeyUp, search }) => (
   <div id="app">
     <div dangerouslySetInnerHTML={{ __html: t("guff") }} />
-    <form id="facebook-pac-browser" onSubmit={e => e.preventDefault()}>
-      <fieldset className="prefabs">
-        <legend>{t("search_terms")}</legend>
-        <ul>
-          {["Trump", "Obama", "Hillary", "Mueller", "Health", "Taxes"].map(
-            term => <Term key={term} search={search} term={term} />
-          )}
-        </ul>
-      </fieldset>
-      <input
-        type="search"
-        id="search"
-        placeholder={t("search")}
-        onChange={onKeyUp}
-      />
-      <Filters />
-    </form>
-    <div className="facebook-pac-ads">
-      {ads.length > 0 ? (
-        <Pagination />
-      ) : (
-        <p className="no_ads">No ads found for {search}.</p>
-      )}
-      <div id="ads">{ads.map(ad => <Ad ad={ad} key={ad.id} />)}</div>
-      {ads.length > 0 ? <Pagination /> : ""}
-    </div>
+    <Switch>
+      <Route exact path="/facebook-ads">
+        {/* confusingly, despite being `exact`, this also matches /facebook-ads/, with the trailing slash */}
+        <div>
+          <form id="facebook-pac-browser" onSubmit={e => e.preventDefault()}>
+            <fieldset className="prefabs">
+              <legend>{t("search_terms")}</legend>
+              <ul>
+                {[
+                  "Trump",
+                  "Obama",
+                  "Hillary",
+                  "Mueller",
+                  "Health",
+                  "Taxes"
+                ].map(term => <Term key={term} search={search} term={term} />)}
+              </ul>
+            </fieldset>
+            <input
+              type="search"
+              id="search"
+              placeholder={t("search")}
+              onChange={onKeyUp}
+            />
+            <Filters />
+          </form>
+          <div className="facebook-pac-ads">
+            {ads.length > 0 ? (
+              <Pagination />
+            ) : (
+              <p className="no_ads">No ads found for {search}.</p>
+            )}
+            <div id="ads">{ads.map(ad => <Ad ad={ad} key={ad.id} />)}</div>
+            {ads.length > 0 ? <Pagination /> : ""}
+          </div>
+        </div>
+      </Route>
+      <Route path="/facebook-ads/ad/:ad_id" component={AdRedux} />
+    </Switch>
   </div>
 );
 
@@ -45,7 +59,7 @@ const throttledDispatch = debounce((dispatch, input) => {
 }, 750);
 
 export default connect(
-  ({ ads, search }) => ({ ads, search }),
+  ({ ads, search, ad }) => ({ ads, search, ad }),
   dispatch => ({
     onKeyUp: e => {
       e.preventDefault();
