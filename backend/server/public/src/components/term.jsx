f5321ae88f1938afdf81ee5328ab5a1d16870709
Jeremy B. Merrill <jeremy.merrill@propublica.org>
2018-02-09 15:57:37 -0500

    adds tests for all the public components

--- backend/server/public/src/components/term.jsx
+++ backend/server/public/src/components/term.jsx
@@ -2,16 +2,22 @@ import React from "react";
 import { connect } from "react-redux";
 import { newSearch } from "actions.js";
 
-let Term = ({ search, term, dispatch }) => (
+export const TermUnconnected = ({ search, term, newSearch }) => (
   <li>
     <button
       type="button"
       className={term === search ? "prefab current" : "prefab"}
-      onClick={() => dispatch(newSearch(term))}
+      onClick={() => newSearch(term)}
       value={term}
     >
       {term}
     </button>
   </li>
 );
-export default connect()(Term);
+const Term = connect(
+  () => {},
+  dispatch => {
+    term => dispatch(newSearch(term));
+  }
+)(TermUnconnected);
+export default Term;
