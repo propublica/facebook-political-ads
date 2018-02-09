ba6a28a9e7f96f76f588e9bd3844514aa5a435fa
Jeremy B. Merrill <jeremy.merrill@propublica.org>
2018-02-09 15:15:39 -0500

    adds tests for ad component

--- backend/server/public/src/components/ad.jsx
+++ backend/server/public/src/components/ad.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Targeting from "./targeting.jsx";
 
 const Ad = ({ ad }) => (
-  <div>
+  <div className="ad">
     <div className="message">
       <div dangerouslySetInnerHTML={{ __html: ad.html }} />
     </div>
