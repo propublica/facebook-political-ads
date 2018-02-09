0e99acb03eeda31cc2aaf560538d224a68a95885
Jeremy B. Merrill <jeremy.merrill@propublica.org>
2018-02-09 14:07:42 -0500

    moves admin component tests to their own dir

--- backend/server/public/__tests__/admin/components_admin_ad.js
+++ backend/server/public/__tests__/admin/components_admin_ad.js
@@ -2,7 +2,7 @@ import React from "react";
 import { StaticRouter } from "react-router-dom";
 import Enzyme, { mount } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
-import { AdminAdUnconnected } from "../src/components/admin/ad.jsx";
+import { AdminAdUnconnected } from "../../src/components/admin/ad.jsx";
 
 Enzyme.configure({ adapter: new Adapter() });
 
