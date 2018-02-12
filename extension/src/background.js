0708ae903c76446d83c59a553d64ad675ec7d4e0
Jeff Larson <thejefflarson@gmail.com>
2018-02-12 14:04:55 -0800

    fix performance regressions

--- extension/src/background.js
+++ extension/src/background.js
@@ -1,24 +1,33 @@
-import { sendAds, mergeAds, updateBadge, adForRequest, getBrowserLocale } from "utils.js";
+import {
+  sendAds,
+  mergeAds,
+  updateBadge,
+  adForRequest,
+  getBrowserLocale
+} from "utils.js";
 
-chrome.runtime.onMessage.addListener((ads) => {
-  if(!localStorage.getItem("redux")) return;
+chrome.runtime.onMessage.addListener(ads => {
+  if (!localStorage.getItem("redux")) return;
   try {
     const store = JSON.parse(localStorage.getItem("redux"));
-    if(!store.terms) return;
+    if (!store.terms) return;
     let saved = new Set();
-    store.ratings.map((ad) => saved.add(ad.id));
+    store.ratings.map(ad => saved.add(ad.id));
     store.ratings = mergeAds(store.ratings || [], ads);
-    localStorage.setItem('redux', JSON.stringify(store));
+    localStorage.setItem("redux", JSON.stringify(store));
     updateBadge(store.ratings);
-    const saving = ads.filter((ad) => !saved.has(ad.id)).map(adForRequest);
+    const saving = ads.filter(ad => !saved.has(ad.id)).map(adForRequest);
     const success = () => console.log("saved");
-    const failure = (e) => console.log(e);
-    if(saving.length > 0) {
-      sendAds(saving, store.language || getBrowserLocale())
-        .then(success, failure);
+    const failure = e => console.log(e);
+    if (saving.length > 0) {
+      sendAds(saving, store.language || getBrowserLocale()).then(
+        success,
+        failure
+      );
     }
-  } catch(e) {
+  } catch (e) {
     console.log(e);
   }
 });
-chrome.browserAction.setBadgeBackgroundColor({color: "#0099E6"});
+
+chrome.browserAction.setBadgeBackgroundColor({ color: "#0099E6" });
--- extension/src/content.js
+++ extension/src/content.js
@@ -8,6 +8,8 @@ const sendAds = function() {
   let posts = Array.from(document.querySelectorAll(SIDEBAR_SELECTOR)).concat(
     Array.from(document.querySelectorAll(TIMELINE_SELECTOR))
   );
+  posts = Array.from(document.querySelectorAll(TIMELINE_SELECTOR));
+
   let results = [];
   let scraper = posts.reduce(
     (p, i) =>
