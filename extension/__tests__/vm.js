3cb6efbdb34699bfa808c1f2e459b34177bcd5c0
Jeff Larson <thejefflarson@gmail.com>
2018-04-03 18:06:40 -0700

    initial vm implementation

--- extension/__tests__/vm.js
+++ extension/__tests__/vm.js
@@ -1,5 +1,55 @@
 import { readFileSync } from "fs";
 
+class AsyncRequest {
+  constructor() {
+    this.data = {};
+    this.uri = { addQueryData: () => "" };
+    this.calls = [];
+  }
+
+  setURI() {
+    this.calls.push("setURI");
+    return this;
+  }
+
+  setData() {
+    this.calls.push;
+    return this;
+  }
+
+  setMethod() {
+    return this;
+  }
+
+  setRelativeTo() {
+    return this;
+  }
+
+  setNectarModuleDataSafe() {
+    return this;
+  }
+
+  setReadOnly() {
+    return this;
+  }
+
+  _setUserActionID() {}
+
+  setNewSerial() {}
+}
+
+globals.AsyncRequest = AsyncRequest;
+Object.assign(globals.window, { require: () => () => {} });
+
+class XMLHttpRequest {
+  open() {}
+  send() {
+    this.readyState = 4;
+    this.onreadystatechange();
+  }
+}
+globals.XMLHttpRequest = XMLHttpRequest;
+
 export class FacebookVM {
   constructor() {
     document.body.innerHTML = `
@@ -12,7 +62,6 @@ export class FacebookVM {
     `;
 
     this.click = () => this._click();
-    this.req = () => this._req();
     this.timelineAd = document.querySelector(
       "#hyperfeed_story_id_5ac2bbb055fd90225091584 .toggle"
     );
@@ -20,9 +69,11 @@ export class FacebookVM {
     this.timelineAd.addEventListener("click", this.click);
   }
 
-  _click() {}
-
-  _req() {}
+  _click() {
+    const node = document.createElement("div");
+    div.innerHTML = readFileSync(__dirname + "/fixtures/toggle.html");
+    document.body.appendChild(node);
+  }
 
   detach() {
     this.timelineAd.removeEventListener("click", this.click);
