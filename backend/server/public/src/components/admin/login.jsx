22fc29afc008b9af900965033f269226a503ce1a
Jeremy B. Merrill <jeremy.merrill@propublica.org>
2018-04-10 12:50:51 -0400

    factor out login/auth/credentials

--- backend/server/public/src/components/admin/login.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { connect } from "react-redux";
-
-export const LoginUnconnected = ({}) => {
-  return <div />;
-};
-const Login = connect(() => ({}), dispatch => ({}))(LoginUnconnected);
-export default Login;
