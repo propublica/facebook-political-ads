30883bdfeb6bc624c0f4b59aec7b02f8ae3e4fea
Jeremy B. Merrill <jeremy.merrill@propublica.org>
2018-04-10 14:41:11 -0400

    handle languages right

--- backend/server/public/src/utils.js
+++ backend/server/public/src/utils.js
@@ -58,14 +58,14 @@ const serialize = state => {
     params.set("maxpoliprob", state.politicalProbability[1]);
   }
 
-  if (state.lang !== i18next.language) {
+  if (state.lang && state.lang !== i18next.language) {
     params.set("lang", state.lang);
   }
 
   return params;
 };
 
-const deserialize = dispatch => {
+const deserialize = (dispatch, forceLang) => {
   const params = new URLSearchParams(location.search);
   const actions = [];
   if (params.has("search")) {
@@ -116,7 +116,16 @@ const deserialize = dispatch => {
     actions.push(newSearch(""));
   }
 
-  actions.push(setLang(params.get("lang") || "en-US"));
+  // if the forceLang param is set (it'll be set to en-US)
+  // then we're goign to give you en-US ads, no matter what
+  // otherwise -- only in admin -- we'll give you the lang you asked for
+  // or the lang your browser asks for.
+  if (
+    forceLang ||
+    (params.get("lang") && params.get("lang") !== i18next.language)
+  ) {
+    actions.push(setLang(forceLang || params.get("lang")));
+  }
   return dispatch(batch(...actions));
 };
 
