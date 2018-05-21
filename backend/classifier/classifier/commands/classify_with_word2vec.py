f2b7039f24079a30dcd8012b2e2e0c126806b91b
Jeremy B. Merrill <jeremy.merrill@propublica.org>
2018-05-21 14:00:35 -0400

    try Canada with word2vec; it works poorly

--- backend/classifier/classifier/commands/classify_with_word2vec.py
+++ backend/classifier/classifier/commands/classify_with_word2vec.py
@@ -5,24 +5,32 @@ import click
 import dill
 from classifier.utilities import (classifier_path, get_vectorizer,
                                   confs, DB, get_text)
+from gensim import Doc2Vec
+from sklearn.ensemble import ExtraTreesClassifier
 
 
-@click.command("classify")
+@click.command("classifyw2v")
 @click.option("--newest/--every",
               default=True,
               help="Classify all of the records")
 @click.option("--lang", help="Limit to language")
 @click.pass_context
-def classify(ctx, newest, lang):
+def classifyw2v(ctx, newest, lang):
     """
     Classify the ads in the database at $DATABASE_URL.
     """
+
+    model = load_model("/Users/jm-admin/code/congress2/lib/word2vec/models/model_doc2vec_communications_5_100_0.025_just_member_id_trigrams_dbow.model")
+
+
+
+
     classifiers = dict()
     for (directory, conf) in confs(ctx.obj["base"]):
         with open(classifier_path(directory), 'rb') as classy:
             classifiers[conf["language"]] = {
                 "classifier": dill.load(classy),
-                "vectorizer": lambda text: get_vectorizer(conf)(text)
+                "vectorizer": lambda texts: model.infer_vector(tokenize_words(texts[0]))
             }
 
     if newest:
@@ -53,7 +61,7 @@ def classify(ctx, newest, lang):
         record_lang = "en-US" if record["lang"] == "en-IE" else record["lang"]
         if record_lang in classifiers:
             classifier = classifiers[record_lang]
-            text = classifier["vectorizer"]([get_text(record)])
+            text = classifier["vectorizer"].transform([get_text(record)])
             probability = classifier["classifier"].predict_proba(text)[0][1]
             update = {
                 "id": record["id"],
@@ -71,3 +79,39 @@ def classify(ctx, newest, lang):
 
     if updates:
         DB.bulk_query(query, updates)
+
+
+
+from tokenizer import TreebankWordTokenizer
+treebank_word_tokenizer = TreebankWordTokenizer()
+alpha_re = re.compile("^[a-zA-Z]+'?[a-zA-Z]*?$") # allow single apostrophes but not double apostrophes: note, this doesn't allow 'ere
+def clean(text):
+    text = text.replace("\xa0", " ") # turns NBSPs into regular spaces (was a problem in Del Kilili's statements)
+                                     # but we do want to preserve line beraks
+    # text = text.encode("ascii","ignore").decode("utf-8") # this is bad, nukes characters from Luján, etc.'s names
+    text = text.replace("“", "\"").replace("”", "\"")
+    text = re.sub(r"([a-z]+)\.([A-Z][a-z]+)", "\\1. \\2", text)
+    text = text.replace("\\r\\n", "\r\n" )
+    text = re.sub(r"# ?# ?#", "", text)
+
+    text = re.sub(r"\t[A-Za-z]+( [A-Za-z]+)?\n\t", "", text) # removes one or two words set off by tabs, since they're probably site chrome
+    return text
+def tokenize_words(text):
+    return [s.lower() for s in treebank_word_tokenizer.tokenize(clean(text)) if re.match(alpha_re, s)]
+
+
+def load_model(args_dot_model):
+  if args_dot_model:
+    model_name = args_dot_model
+  else:
+    if not exists(most_recent_model_filename_filename):
+      print("Oops! You need to train a model before you can use this function (or specify the location of your model with the --model flag).")
+      print("In most cases, running `python train_doc2vec_model.py inputs/your/input/folder` should do the trick!")
+      exit(1)
+    with open(most_recent_model_filename_filename, "r") as f:
+        model_name =  (f.read().strip())
+    if not exists(model_name):
+      print("Oops! Your model may have been deleted or moved. Either specify its location with the --model flag or fix the path in .texttoolkit_model_filename (or train a new model).")
+      exit(1)
+  return Doc2Vec.load(model_name)
+
--- backend/classifier/classifier/utilities.py
+++ backend/classifier/classifier/utilities.py
@@ -14,7 +14,9 @@ from sklearn.feature_extraction.text import HashingVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 from sqlalchemy.sql import text
-
+import re
+from numpy import matrix
+from sklearn.preprocessing import minmax_scale
 DB = records.Database()
 CLASSIFIER = "MultinomialNB"
 
@@ -33,11 +35,18 @@ def get_vectorizer(conf):
     Return a HashingVectorizer, which we're using so that we don't
     need to serialize one.
     """
-    return HashingVectorizer(
-        alternate_sign=False,
-        n_features=conf["n_features"],
-        ngram_range=(1, 3)
-    )
+    global d2v_model
+    if conf.get("vectorizer_type", None) == "doc2vec":
+        print("loading d2v model...")
+        d2v_model = load_model("/Users/jm-admin/code/congress2/lib/word2vec/models/model_doc2vec_communications_5_100_0.025_just_member_id_trigrams_dbow.model")
+
+    return (lambda text: HashingVectorizer(
+            alternate_sign=False,
+            n_features=conf["n_features"],
+            ngram_range=(1, 3)
+        ).transform(text)) if conf.get("vectorizer_type", None) != "doc2vec" else (lambda texts: minmax_scale(matrix(list(map(
+        lambda text: d2v_model.infer_vector(tokenize_words(text)),
+        texts)))))
 
 
 def get_classifier():
@@ -71,10 +80,15 @@ def train_classifier(classifier, vectorizer, base, language):
     data.extend(load_ads_from_psql(language))
     print("num unique samples: {}".format(len(data)))
     train, test = train_test_split(data, test_size=0.1)
-    x_train, y_train = zip(*train)
+    x_train, y_train = zip(*train) # x_train is a tuple of strings.
     x_test, y_test = zip(*test)
-    x_train = vectorizer.transform(x_train)
-    x_test = vectorizer.transform(x_test)
+    print(type(x_train))
+    print(repr(x_train)[0:200])
+    print(vectorizer)
+    x_train = vectorizer(x_train) # x_train is now a scipy.sparse.csr.csr_matrix; sparse matrix of type numpy.float64
+    x_test = vectorizer(x_test)
+    print(type(x_train))
+    print(repr(x_train)[0:200])
     x_train, y_train = equalize_classes(x_train, y_train)
     print("final size of training data: %s" % x_train.shape[0])
     classifier.fit(x_train, y_train)
@@ -181,3 +195,40 @@ def load_ads_from_psql(lang):
             score = round(advert["score"])
         data.append((get_text(advert), score))
     return data
+
+
+global d2v_model
+
+from gensim.models import Doc2Vec
+from tokenizer import TreebankWordTokenizer
+treebank_word_tokenizer = TreebankWordTokenizer()
+alpha_re = re.compile("^[a-zA-Z]+'?[a-zA-Z]*?$") # allow single apostrophes but not double apostrophes: note, this doesn't allow 'ere
+def clean(text):
+    text = text.replace("\xa0", " ") # turns NBSPs into regular spaces (was a problem in Del Kilili's statements)
+                                     # but we do want to preserve line beraks
+    # text = text.encode("ascii","ignore").decode("utf-8") # this is bad, nukes characters from Luján, etc.'s names
+    text = text.replace("“", "\"").replace("”", "\"")
+    text = re.sub(r"([a-z]+)\.([A-Z][a-z]+)", "\\1. \\2", text)
+    text = text.replace("\\r\\n", "\r\n" )
+    text = re.sub(r"# ?# ?#", "", text)
+
+    text = re.sub(r"\t[A-Za-z]+( [A-Za-z]+)?\n\t", "", text) # removes one or two words set off by tabs, since they're probably site chrome
+    return text
+def tokenize_words(text):
+    return [s.lower() for s in treebank_word_tokenizer.tokenize(clean(text)) if re.match(alpha_re, s)]
+
+
+def load_model(args_dot_model):
+  if args_dot_model:
+    model_name = args_dot_model
+  else:
+    if not exists(most_recent_model_filename_filename):
+      print("Oops! You need to train a model before you can use this function (or specify the location of your model with the --model flag).")
+      print("In most cases, running `python train_doc2vec_model.py inputs/your/input/folder` should do the trick!")
+      exit(1)
+    with open(most_recent_model_filename_filename, "r") as f:
+        model_name =  (f.read().strip())
+    if not exists(model_name):
+      print("Oops! Your model may have been deleted or moved. Either specify its location with the --model flag or fix the path in .texttoolkit_model_filename (or train a new model).")
+      exit(1)
+  return Doc2Vec.load(model_name)
