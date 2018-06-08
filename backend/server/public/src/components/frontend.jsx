37fa339c8091cc8bca3458ef64d3cffae90e8300
Jeremy B. Merrill <jeremy.merrill@propublica.org>
2018-06-08 17:28:29 -0400

    adds Which Ads? thing to the bottom of the page

--- backend/server/public/src/components/frontend.jsx
+++ backend/server/public/src/components/frontend.jsx
@@ -13,6 +13,7 @@ export const FrontEnd = () => (
       </Route>
       <Route path="/facebook-ads/ad/:ad_id" component={AdDetail} />
     </Switch>
+    <div id="which-ads" dangerouslySetInnerHTML={{ __html: t("whichads") }} />
   </div>
 );
 
