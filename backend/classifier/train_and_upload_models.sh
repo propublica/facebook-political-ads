365644920ef3a449c05b0996f2ca1bf9dc1aa6af
Jeremy B. Merrill <jeremy.merrill@propublica.org>
2018-06-21 17:43:06 -0400

    es-MX classifier (that doesn't work well yet, but, bekol zaman)

--- backend/classifier/train_and_upload_models.sh
+++ backend/classifier/train_and_upload_models.sh
@@ -1,4 +1,4 @@
-for langCode in "es-ES" "de-AT" "en-CA" "nl-BE" "da-DK" "en-AU" "en-IE" "it-IT" "de-DE" "en-US" "fr-CA" "sv-SE" "fi-FI" "de-CH" "nl-NL"; do
+for langCode in "es-MX" "de-AT" "en-CA" "nl-BE" "da-DK" "en-AU" "en-IE" "it-IT" "de-DE" "en-US" "fr-CA" "sv-SE" "fi-FI" "de-CH" "nl-NL"; do
 	/usr/local/bin/pipenv run ./classify build --lang $langCode
 	aws s3 cp --acl public-read data/$langCode/classifier.dill s3://pp-data/fbpac-models/$langCode/classifier.dill
 done
