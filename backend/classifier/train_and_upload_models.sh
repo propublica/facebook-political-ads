c9bcac4e7d42af29ba2d0abc88d797bc5810e4a3
Jeremy B. Merrill <jeremy.merrill@propublica.org>
2018-06-21 15:27:32 -0400

    updates Pipfile

--- backend/classifier/train_and_upload_models.sh
+++ backend/classifier/train_and_upload_models.sh
@@ -1,5 +1,5 @@
 /usr/local/bin/pipenv run ./classify build
-for langCode in "es-ES" "de-AT" "en-CA" "nl-BE" "da-DK" "en-AU" "it-IT" "de-DE" "en-US" "fr-CA" "sv-SE" "fi-FI" "de-CH" "nl-NL"; do
+for langCode in "es-ES" "de-AT" "en-CA" "nl-BE" "da-DK" "en-AU" "en-IE" "it-IT" "de-DE" "en-US" "fr-CA" "sv-SE" "fi-FI" "de-CH" "nl-NL"; do
 	aws s3 cp --acl public-read data/$langCode/classifier.dill s3://pp-data/fbpac-models/$langCode/classifier.dill
 done
 
