68f4aac245d4eba96e08f89a60164acf7ec34e6f
Jeremy B. Merrill <jeremy.merrill@propublica.org>
2018-06-21 11:10:24 -0400

    adds a script to train the models

---
+++ backend/classifier/train_and_upload_models.sh
@@ -0,0 +1,5 @@
+pipenv run ./classify build
+for langCode in "es-ES" "de-AT" "en-CA" "nl-BE" "da-DK" "en-AU" "it-IT" "de-DE" "en-US" "fr-CA" "sv-SE" "fi-FI" "de-CH" "nl-NL"; do
+	aws s3 cp --acl public-read data/$langCode/classifier.dill s3://pp-data/fbpac-models/$langCode/classifier.dill
+done
+
