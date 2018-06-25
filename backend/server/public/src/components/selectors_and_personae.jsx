572b72e60fa74c2292516046cccc16abfaa91a35
Jeremy B. Merrill <jeremy.merrill@propublica.org>
2018-06-25 16:47:42 -0400

    incremental improvement in mockup

--- backend/server/public/src/components/selectors_and_personae.jsx
+++ backend/server/public/src/components/selectors_and_personae.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 import Persona from "components/persona.jsx";
 
-const FbpacFeltrons = () => (
-  <div>
+const SelectorsAndPersonae = () => (
+  <div className="selectorsAndPersonae">
     <p>
-      Fill out your infomration to see who is targeting a 48 year old male
-      living in Ohio and who is conservative.
+      Fill out your infomration to see who is targeting a{" "}
+      <select>
+        <option>48 year old</option>
+      </select>
+      <select>
+        <option>male</option>
+      </select>
+      living in{" "}
+      <select>
+        <option>Ohio</option>
+      </select>{" "}
+      and who is{" "}
+      <select>
+        <option>conservative</option>
+      </select>.
     </p>
     <div>
       <p>Or see who is targeting someone like</p>
@@ -20,4 +33,4 @@ const FbpacFeltrons = () => (
   </div>
 );
 
-export default FbpacFeltrons;
+export default SelectorsAndPersonae;
