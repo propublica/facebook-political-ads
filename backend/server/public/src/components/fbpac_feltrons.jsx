2430ff1a67538664a70872be3d1dd63801f8334b
Jeremy B. Merrill <jeremy.merrill@propublica.org>
2018-06-25 15:16:22 -0400

    a little more details

--- backend/server/public/src/components/fbpac_feltrons.jsx
+++ backend/server/public/src/components/fbpac_feltrons.jsx
@@ -2,7 +2,12 @@ import React from "react";
 
 const FbpacFeltrons = () => (
   <div>
-    <strong>Feltrons</strong>: Collected, targeted counts, etc.
+    <span>
+      <strong>Feltrons go here</strong>: Collected, targeted counts, etc.
+    </span>
+    <div>123 ads today | 12345 ads overall</div>
+    <div>~10% of US Facebook ads are political</div>
+    <div>9500 users.</div>
   </div>
 );
 
