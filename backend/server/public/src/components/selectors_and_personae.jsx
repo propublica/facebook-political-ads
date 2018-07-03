3936412c5e575e5e0277a9469eea28fc6ed39637
Jeremy B. Merrill <jeremy.merrill@propublica.org>
2018-07-03 14:44:02 -0400

    states

--- backend/server/public/src/components/selectors_and_personae.jsx
+++ backend/server/public/src/components/selectors_and_personae.jsx
@@ -38,10 +38,56 @@ const SelectorsAndPersonae = ({ getAds, setPersonaFacet, persona }) => (
         }}
       >
         <option>Ohio</option>
-        <option>Long Island, NY</option>
-        <option>big city</option>
-        <option>suburbs</option>
-        <option>rural America</option>
+        <option>Alabama</option>
+        <option>Alaska</option>
+        <option>Arizona</option>
+        <option>Arkansas</option>
+        <option>California</option>
+        <option>Colorado</option>
+        <option>Connecticut</option>
+        <option>Delaware</option>
+        <option>Florida</option>
+        <option>Georgia</option>
+        <option>Hawaii</option>
+        <option>Idaho</option>
+        <option>Illinois</option>
+        <option>Indiana</option>
+        <option>Iowa</option>
+        <option>Kansas</option>
+        <option>Kentucky</option>
+        <option>Louisiana</option>
+        <option>Maine</option>
+        <option>Maryland</option>
+        <option>Massachusetts</option>
+        <option>Michigan</option>
+        <option>Minnesota</option>
+        <option>Mississippi</option>
+        <option>Missouri</option>
+        <option>Montana</option>
+        <option>Nebraska</option>
+        <option>Nevada</option>
+        <option>New Hampshire</option>
+        <option>New Jersey</option>
+        <option>New Mexico</option>
+        <option>New York</option>
+        <option>North Carolina</option>
+        <option>North Dakota</option>
+        <option>Ohio</option>
+        <option>Oklahoma</option>
+        <option>Oregon</option>
+        <option>Pennsylvania</option>
+        <option>Rhode Island</option>
+        <option>South Carolina</option>
+        <option>South Dakota</option>
+        <option>Tennessee</option>
+        <option>Texas</option>
+        <option>Utah</option>
+        <option>Vermont</option>
+        <option>Virginia</option>
+        <option>Washington</option>
+        <option>West Virginia</option>
+        <option>Wisconsin</option>
+        <option>Wyoming</option>
       </select>{" "}
       and who is{" "}
       <select
@@ -88,7 +134,7 @@ const SelectorsAndPersonae = ({ getAds, setPersonaFacet, persona }) => (
               cities: "Long Island",
               state: "NY"
             },
-            name: 'William "Billy" Joel'
+            name: "William \"Billy\" Joel"
           },
           {
             age: "28",
