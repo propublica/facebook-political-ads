c535e65403fb6efe0e0d958c243cbd6119ef3ea2
Jeremy B. Merrill <jeremy.merrill@propublica.org>
2018-07-06 11:58:06 -0400

    switch up states to show Please Install thingy per new analysis

--- backend/server/public/src/components/please_install.jsx
+++ backend/server/public/src/components/please_install.jsx
@@ -5,78 +5,81 @@ const PleaseInstallUnconnected = ({ persona }) =>
   persona &&
   persona.location &&
   ([
-    "TX",
-    "IA",
-    "VA",
     "NC",
-    "WV",
-    "AZ",
-    "IL",
-    "FL",
     "PA",
-    "CN",
-    "OH",
-    "TN",
-    "IN",
-    "KY",
-    "UT",
-    "AL",
-    "LA",
+    "TX",
+    "CT",
+    "MI",
+    "IL",
+    "WY",
+    "FL",
+    "MT",
+    "NJ",
+    "CO",
+    "AZ",
+    "OK",
+    "IA",
     "GA",
+    "OH",
+    "DE",
+    "UT",
+    "IN",
+    "TN",
+    "AL",
+    "KY",
+    "LA",
     "NV",
     "SD",
-    "DE",
-    "NH",
-    "VT",
-    "AR",
-    "HI",
     "ID",
-    "SC",
+    "HI",
+    "AR",
     "RI",
-    "KS"
+    "KS",
+    "SC",
+    "MS"
   ].indexOf(persona.location[0]) > -1 ||
     persona.politics == "conservative") ? (
-      <div className="glad-you-here">
-        <div>
-          <img
-            src="https://propublica.s3.amazonaws.com/assets/images/headshot.jpg"
-            alt="headshot"
-          />
-        </div>
-        <div>
-          <h2>Is this you? We need your help!</h2>
-          <h4>
+    <div className="glad-you-here">
+      <div>
+        <img
+          src="https://propublica.s3.amazonaws.com/assets/images/headshot.jpg"
+          alt="headshot"
+        />
+      </div>
+      <div>
+        <h2>Is this you? We need your help!</h2>
+        <h4>
           Not many people with the traits you selected are using our political
           ad collector.
-          </h4>
-          <p>
+        </h4>
+        <p>
           This means even though the ads are out there, we don't have them in
           our database. Is this you? Help us help other people like you by using
           the ad collector.
-          </p>
-          <a href="#">Use the ad collector</a>
-        </div>
+        </p>
+        <a href="#">Use the ad collector</a>
       </div>
-    ) : (
-      <div className="glad-you-here">
-        <div>
-        <img
-            src="https://propublica.s3.amazonaws.com/assets/images/headshot.jpg"
-            alt="headshot"
-        />
-        </div>
+    </div>
+  ) : (
+    <div className="glad-you-here">
       <div>
-        <h2>Hey, we're glad you're here!</h2>
-          <h4>You can help us make our ad database more representative.</h4>
-        <p>
+          <img
+          src="https://propublica.s3.amazonaws.com/assets/images/headshot.jpg"
+          alt="headshot"
+          />
+      </div>
+        <div>
+          <h2>Hey, we're glad you're here!</h2>
+        <h4>You can help us make our ad database more representative.</h4>
+          <p>
           The political ad collector only receives Facebook ads from people
           using the browser extension. Help us help other people like you by
           using the ad collector.
-          </p>
-          <a href="#">Use the ad collector</a>
-      </div>
-      </div>
-    );
+        </p>
+        <a href="#">Use the ad collector</a>
+        </div>
+    </div>
+  );
 
 const PleaseInstall = connect(
   ({ persona }) => ({
