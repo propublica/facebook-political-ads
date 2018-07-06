102aa7cb7153eade57ded1b44e9ab5dbd0dbf30e
Ally Levine <allyjlevine@gmail.com>
2018-07-06 15:21:59 -0400

    styling feltrons

--- backend/server/public/src/components/fbpac_feltrons.jsx
+++ backend/server/public/src/components/fbpac_feltrons.jsx
@@ -3,51 +3,47 @@ import { connect } from "react-redux";
 import PoliticalRatioChart from "components/political_ratio_chart.jsx";
 
 const FbpacFeltronsUnconnected = ({ homepage_stats }) => (
-  <div className="feltrons flexy">
-    <div className="third">
-      <h3>Political Ads Collected</h3>
-      <div className="flexy">
-        <div className="half">
-          <span className="feltron">
-            {homepage_stats ? homepage_stats.political_ads_today : "--"}
-          </span>
-          <p> ads today</p>
-        </div>
-        <div className="half">
-          <span className="feltron">
-            {homepage_stats && homepage_stats.political_ads_total
-              ? homepage_stats.political_ads_total
-                .toString()
-                .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
-              : "--"}
-          </span>
-          <p>ads overall</p>
+  <div className="feltrons">
+    <div className="top-feltrons-container">
+      <div className="top-feltrons political-ratio">
+        <h3>More Political Ads Lately?</h3>
+        <PoliticalRatioChart
+          weekly_political_ratio={
+            homepage_stats && homepage_stats.weekly_political_ratio
+              ? homepage_stats.weekly_political_ratio.slice(
+                  0,
+                  homepage_stats.weekly_political_ratio.length - 1
+                )
+              : null
+          }
+        />
+      </div>
+      <div className="top-feltrons">
+        <h3>Top targeted traits</h3>
+        <div className="flexy">
+          <div className="">
+            <p>age</p>
+            <p>and</p>
+            <p>region</p>
+          </div>
         </div>
       </div>
     </div>
-    <div className="third political-ratio">
-      <h3>More Political Ads Lately?</h3>
-      <PoliticalRatioChart
-        weekly_political_ratio={
-          homepage_stats && homepage_stats.weekly_political_ratio
-            ? homepage_stats.weekly_political_ratio.slice(
-                0,
-                homepage_stats.weekly_political_ratio.length - 1
-              )
-            : null
-        }
-      />
-    </div>
-    <div className="third">
-      <h3>Contributors</h3>
-      <span className="feltron">
-        {homepage_stats && homepage_stats.user_count
+    <div className="stats-sentence">
+      <p>
+        <strong>{homepage_stats && homepage_stats.user_count
           ? homepage_stats.user_count
             .toString()
             .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
-          : "--"}
-      </span>
-      <p>all readers like you!</p>
+          : "--"}</strong>
+         {" "}readers like you have helped collect{" "}
+        <strong>{homepage_stats && homepage_stats.political_ads_total
+          ? homepage_stats.political_ads_total
+            .toString()
+            .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+          : "--"}</strong>
+        {" "}political ads.
+      </p>
     </div>
   </div>
 );
