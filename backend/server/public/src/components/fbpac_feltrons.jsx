0aaa0be20ac14eca2d49324730a142f2c917b4d6
Ally Levine <allyjlevine@gmail.com>
2018-07-17 10:21:06 -0400

    top bar desktop and ipad styling

--- backend/server/public/src/components/fbpac_feltrons.jsx
+++ backend/server/public/src/components/fbpac_feltrons.jsx
@@ -6,7 +6,7 @@ const FbpacFeltronsUnconnected = ({ homepage_stats }) => (
   <div className="feltrons">
     <div className="top-feltrons-container">
       <div className="top-feltrons political-ratio">
-        <h3>This Week's Political Ads</h3>
+        <h3>This Week</h3>
         <PoliticalAdsChart
           political_ads_per_day={
             homepage_stats && homepage_stats.political_ads_per_day
@@ -19,25 +19,25 @@ const FbpacFeltronsUnconnected = ({ homepage_stats }) => (
         />
       </div>
       <div className="top-feltrons top-target">
-        <h3>Top targeted methods</h3>
-        <p>location &#8226; politics &#8226; custom audience &#8226; age &#8226; look-a-like audience</p>
+        <h3>Top Targeting Methods</h3>
+        <p>location, political affiliation, custom&nbsp;audience</p>
       </div>
       <div className="top-feltrons ads-collected">
-        <h3>Total Political Ads</h3>
-        <p><strong>
-          {homepage_stats && homepage_stats.user_count
-            ? homepage_stats.user_count
-                .toString()
-                .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
-            : "--"}
-        </strong>{" "}contributors collected</p>
+        <h3>Political Ads Collected</h3>
         <p><strong>
           {homepage_stats && homepage_stats.political_ads_total
             ? homepage_stats.political_ads_total
                 .toString()
                 .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
             : "--"}
-        </strong>{" "} political ads</p>
+        </strong>{" "} ads from</p>
+        <p><strong>
+          {homepage_stats && homepage_stats.user_count
+            ? homepage_stats.user_count
+                .toString()
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+            : "--"}
+        </strong>{" "}contributors</p>
       </div>
     </div>
   </div>
