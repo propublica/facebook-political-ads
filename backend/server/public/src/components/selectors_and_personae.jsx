0d21326c5722c2a0b016ef79572336b07761b3c2
Ally Levine <allyjlevine@gmail.com>
2018-07-17 15:55:34 -0400

    removed sticky attributes from selector sentence via css

--- backend/server/public/src/components/selectors_and_personae.jsx
+++ backend/server/public/src/components/selectors_and_personae.jsx
@@ -6,19 +6,17 @@ import Select from "react-select";
 
 export class SelectorsAndPersonae extends React.Component {
   componentDidUpdate() {
-    if (window.innerWidth > 480) {
-      var el = document.getElementById("sticky_nav");
-      var eloffset = el.offsetTop;
+    var el = document.getElementById("sticky_nav");
+    var eloffset = el.offsetTop;
 
-      window.onscroll = function() {
-        var windowTop = window.scrollY;
-        if (windowTop > eloffset) {
-          el.classList.add("sticky");
-        } else {
-          el.classList.remove("sticky");
-        }
-      };
-    }
+    window.onscroll = function() {
+      var windowTop = window.scrollY;
+      if (windowTop > eloffset) {
+        el.classList.add("sticky");
+      } else {
+        el.classList.remove("sticky");
+      }
+    };
   }
 
   render() {
