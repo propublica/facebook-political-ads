7918ee3cd93e558772ddac1b037231ab82b03067
Jeremy B. Merrill <jeremy.merrill@propublica.org>
2018-08-09 16:51:06 -0400

    updates methodology, makes 'em collapsible

---
+++ backend/server/public/src/components/bottom-guff-item.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Collapse } from "react-collapse";
+
+class BottomGuffItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      shown: props.shown
+    };
+  }
+  render() {
+    return (
+      <div className="bottom-guff">
+        <h3
+          dangerouslySetInnerHTML={{
+            __html: this.props.titleHtml + (this.state.shown ? " -" : " +")
+          }}
+          onClick={() => this.setState({ shown: !this.state.shown })}
+        />
+        <Collapse isOpened={this.state.shown}>
+          <div dangerouslySetInnerHTML={{ __html: this.props.bodyHtml }} />
+        </Collapse>
+      </div>
+    );
+  }
+}
+
+export default BottomGuffItem;
