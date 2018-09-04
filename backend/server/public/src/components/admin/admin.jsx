bd462b6f81d77b983383ebafc50ac120a04b39a8
Jeremy B. Merrill <jeremy.merrill@propublica.org>
2018-09-04 12:09:27 -0400

    fix routing on the v slow state page

--- backend/server/public/src/components/admin/admin.jsx
+++ backend/server/public/src/components/admin/admin.jsx
@@ -42,7 +42,7 @@ export const LoggedInApp = () => {
       <Route path="/facebook-ads/admin/help" component={AdminHelp} />
       <Route path="/facebook-ads/admin/summary" component={Summary} />
       <Route path="/facebook-ads/admin/states" component={StatesDistricts} />
-      <Route path="/facebook-ads/admin/by_state" component={Ads} />
+      <Route path="/facebook-ads/admin/by_state/:state" component={Ads} />
     </div>
   );
 };
